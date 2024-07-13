bd52f5610e61cbd4d90ee7086f1460f9ba10b787
Mehar-Aziz <meharaziz.1614>
2024-07-13 17:53:05 +0500

    Inputs taken

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome to pythin Pizza Deliveries!")
+size = input("Which size pizza do you want? S, M or L: ")
+add_pepperoni = input("Do you want to add pepperoni? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+
