ece4e5bf3ca493bce2a0936bada17239c926a168
Mehar-Aziz <meharaziz.1614>
2024-07-13 18:05:11 +0500

    Update code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-print("Welcome to pythin Pizza Deliveries!")
-size = input("Which size pizza do you want? S, M or L: ")
+print("Welcome to Python Pizza Deliveries!")
+size = input("Which size pizza do you want to order? S, M or L: ")
 add_pepperoni = input("Do you want to add pepperoni? Y or N: ")
-extra_cheese = input("Do you want extra cheese? Y or N: ")
+extra_cheese = input("Do you want some extra cheese? Y or N: ")
 price = 0
 if size == "S":
     price= 15
